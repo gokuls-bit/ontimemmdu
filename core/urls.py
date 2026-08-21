1e040893817f33ba4744169947c5a11a6a73c1c4
Gokul Kumar Sant <gokulsant684@gmail.com>
2026-08-21 11:26:31 +0530

    at the last   for the betterment with modified  6  mastered

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import download_timetable_view
+from .views import download_timetable_view, student_app_view
 
 urlpatterns = [
     path('timetable/download/<str:semester>/<str:fmt>/', download_timetable_view, name='download_timetable'),
+    path('', student_app_view, name='student_app'),
 ]
