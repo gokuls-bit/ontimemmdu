08cd2fe96a52d94f4299eef8b16d407297435a24
Gokul Kumar Sant <gokulsant684@gmail.com>
2026-08-21 11:39:33 +0530

    updated url.py

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/static/frontend/',
   build: {
     outDir: path.resolve(__dirname, '../static/frontend'),
     emptyOutDir: true,
--- smartroom/urls.py
+++ smartroom/urls.py
@@ -1,10 +1,25 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.static import serve
+import os
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('core.api.urls')),
     path('timetable-legacy/', include('timetable.urls')),
+
+    # Serve static assets directly for frontend bundle
+    re_path(r'^assets/(?P<path>.*)$', serve, {
+        'document_root': os.path.join(settings.BASE_DIR, 'static', 'frontend', 'assets'),
+    }),
+    re_path(r'^static/frontend/assets/(?P<path>.*)$', serve, {
+        'document_root': os.path.join(settings.BASE_DIR, 'static', 'frontend', 'assets'),
+    }),
+
     path('', include('core.urls')),
 ]
 
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
